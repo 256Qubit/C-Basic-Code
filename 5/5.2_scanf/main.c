bacf4720b150acf7fc6f90c52f083bcff39194a5
256Qubit <jmortyrick@gmail.com>
2023-07-08 17:28:05 +0800

    5.2 提交

--- 5/5.2_scanf/main.c
+++ 5/5.2_scanf/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+void print(int a[5],int length){
+    int i;
+    for (int i = 0; i <length; ++i) {
+        printf("%d\n",a[i]);
+    }
+//    printf("%d\n",sizeof(a));//sizeof 字节长度
+}
 int main() {
-    printf("Hello, World!\n");
+    int a[5]={1,2,3,4,5};
+    print(a,5);
     return 0;
 }
