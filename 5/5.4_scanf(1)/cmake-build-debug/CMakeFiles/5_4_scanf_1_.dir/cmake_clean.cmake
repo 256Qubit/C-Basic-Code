4f749f45fea054d2f341f42d87a4caea9c237c9a
256Qubit <jmortyrick@gmail.com>
2023-07-23 16:02:23 +0800

    5.3 提交

---
+++ 5/5.4_scanf(1)/cmake-build-debug/CMakeFiles/5_4_scanf_1_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_4_scanf_1_.exe"
+  "5_4_scanf_1_.exe.manifest"
+  "5_4_scanf_1_.pdb"
+  "CMakeFiles/5_4_scanf_1_.dir/main.c.obj"
+  "lib5_4_scanf_1_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/5_4_scanf_1_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
