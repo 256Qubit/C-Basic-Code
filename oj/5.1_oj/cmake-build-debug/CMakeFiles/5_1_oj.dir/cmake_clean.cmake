727d77f5ec4cdea9743fa51190d4f29434cdd148
256Qubit <jmortyrick@gmail.com>
2023-07-23 16:06:04 +0800

    5.2 提交

---
+++ oj/5.1_oj/cmake-build-debug/CMakeFiles/5_1_oj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_1_oj.exe"
+  "5_1_oj.exe.manifest"
+  "5_1_oj.pdb"
+  "CMakeFiles/5_1_oj.dir/main.c.obj"
+  "lib5_1_oj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/5_1_oj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
