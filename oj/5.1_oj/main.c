d9e3db566f33866250102949feb060ef92b6e478
256Qubit <jmortyrick@gmail.com>
2023-07-23 16:20:56 +0800

    51._oj 提交

--- oj/5.1_oj/main.c
+++ oj/5.1_oj/main.c
@@ -1,6 +1,19 @@
+
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int a[100], n, count = 0;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+
+        scanf("%d", &a[i]);
+
+        if (a[i] == 2) {
+            count++;
+        }
+    }
+    printf("%d\n", count);
 }
