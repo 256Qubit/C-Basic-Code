6a83f7085d6c99bdb427c857a9db07016b904d3e
256Qubit <jmortyrick@gmail.com>
2023-08-14 13:53:10 +0800

    6.4 提交

---
+++ 6/6.4-mallcon/cmake-build-debug/CMakeFiles/6_4_mallcon.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "6_4_mallcon.exe"
+  "6_4_mallcon.exe.manifest"
+  "6_4_mallcon.pdb"
+  "CMakeFiles/6_4_mallcon.dir/main.c.obj"
+  "lib6_4_mallcon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/6_4_mallcon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 6/6.4-mallcon/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int size;
+    scanf("%d\n",&size);
+    char *p;//void*类型是不能偏移的
+    p=(char*)malloc(size);//malloc返回void*是无类型指针
+    p[0]='H';
+    *(p+1)='E';
+    p[2]='L';
+    *(p+3)='L';
+    p[4]='O';
+    *(p+5)='\0';
+    strcpy(p,"malloc success");
+    puts(p);
+    return 0;
+}
